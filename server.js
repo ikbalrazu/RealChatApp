df4ef19b58906e6574b31086207d7b010591ff77
unknown <iqbalraju123@gmail.com>
2022-09-15 20:35:54 +0600

    socket implementation done

--- server.js
+++ server.js
@@ -58,8 +58,53 @@ const io=require('socket.io')(server,{
     },
 });
 
+let activeUsers = [];
+
 io.on('connection',(socket)=>{
-    console.log("a user connected",socket.id);
-    const response = new Date();
-    socket.send(response);
+    // console.log("a user connected",socket.id);
+    // const response = new Date();
+    // socket.send(response);
+
+    //add new user
+    socket.on("new-user-add", (newUserId)=>{
+        //if user is not added previously
+        if(!activeUsers.some((user)=> user.userId === newUserId)){
+            activeUsers.push({userId: newUserId, socketId: socket.id});
+            console.log("New User Connected", activeUsers);
+        }
+        //send all active users to new user
+        io.emit("get-users", activeUsers);
+    });
+
+    socket.on("disconnect",()=>{
+        //remove user from active users
+        activeUsers = activeUsers.filter((user)=> user.socketId !== socket.id);
+        console.log("User Disconnected", activeUsers);
+        //send all active users to all users
+        io.emit("get-users", activeUsers);
+    });
+
+
+    //send message to a specific user
+    socket.on("send-message", (data)=>{
+        const {receiverId} = data;
+        const user = activeUsers.find((user)=>user.userId === receiverId);
+        console.log("Sending from socket to :", receiverId);
+        console.log("Data: ", data);
+        if(user){
+            io.to(user.socketId).emit("recieve-message", data);
+        }
+    });
+    
+
+    //custom event server to client data
+    //socket.emit("myevent", "hello iqbal");
+
+    // socket.on("myevent", function(data){
+    //     console.log(data);
+    // });
+
+    // socket.on("disconnect",()=>{
+    //     console.log("User disconnected", socket.id)
+    // })
 })
