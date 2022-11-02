f7e9fadd3e2a29ff44e47e25b070b686e0bc649f
unknown <iqbalraju123@gmail.com>
2022-11-02 11:27:01 +0600

    login form update

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -6,23 +6,38 @@ import Alert from '@mui/material/Alert';
 import LinearProgress from '@mui/material/LinearProgress';
 import { Link } from 'react-router-dom';
 import {useForm, Controller} from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import './Login.css';
 
 const Login = () => {
-    const chatpage = useNavigate();
-    const [email,setEmail] = useState();
-    const [password,setPassword] = useState();
-    const [alert,setAlert] = useState(false);
-    const [alertContent, setAlertContent] = useState('');
+    // const chatpage = useNavigate();
+    // const [email,setEmail] = useState();
+    // const [password,setPassword] = useState();
+    // const [alert,setAlert] = useState(false);
+    // const [alertContent, setAlertContent] = useState('');
     const [loader,setLoader] = useState(false);
 
-    const {handleSubmit, control, formState:{errors}} = useForm();
-    
-    const LoginUser = async(data) => {
-      e.preventDefault();
+    const SignupSchema = yup.object().shape({
+      email: yup.string().required('Email is required').email('Email is invalid'),
+      password: yup.string()
+      .required('Password is required')
+      .min(8, 'Password must contain at least 8 characters'),
+      // age: yup.number().required().positive().integer(),
+      // website: yup.string().url()
+    });
 
-      console.log("data:",data);
+    const { handleSubmit, control, formState: { errors } } = useForm({
+      resolver: yupResolver(SignupSchema)
+    });
+    
+    const onSubmit = async(value) => {
+
+      console.log("data:",value);
+      setLoader(true);
+      //const data = await axios.post("/user/login",{value.email,valuepassword});
+
         // if (!email || !password) {
         //     console.log("Please fill the all fields");
         //     setAlertContent("Please fill the all fields");
@@ -62,11 +77,17 @@ const Login = () => {
         //     }
         // }
     }
+
+    const style = {
+      margin: "auto",
+      "max-width": "300px"
+    };
     
   return (
-    <div className='container'>
-      <form onSubmit={handleSubmit(LoginUser)}>
+    <div>
+      
       <Box
+      onSubmit={handleSubmit(onSubmit)}
       bgcolor={"white"}
       component="form"
       maxWidth={320}
@@ -86,33 +107,47 @@ const Login = () => {
 
       {/* {alert ? <Alert severity='error'>{alertContent}</Alert> : <></> } */}
 
-      <Controller
-      name="email"
-      control={control}
-      render={({field})=>(
-        <TextField 
-        type={'email'}
-        fullWidth
-        name='email'
-        {...field}
-        // onChange={(e) => setEmail(e.target.value.toLowerCase())} 
-        id="outlined-basic" 
-        label="Email" 
-        variant="outlined"
-        />
-      )}
-      />
 
-      {/* <TextField 
-      type={"password"}
-      fullWidth
-      name='password'
-      {...register('password')}
-      // onChange={(e) => setPassword(e.target.value)} 
-      id="outlined-basic" 
-      label="Password" 
-      variant="outlined"
-      /> */}
+      
+          <Controller
+          name='email'
+          control={control}
+          rules={{required:true}}
+          render={({field})=>(
+              <TextField
+              {...field}
+              label="email"
+              error={!!errors['email']}
+              helperText={errors['email'] ? errors['email'].message : ''}
+              // helperText={errors?.email ? errors?.email?.message : ""}
+              type="email"
+              fullWidth
+              />
+          )}
+          />
+
+          <Controller
+          name='password'
+          control={control}
+          rules={{required:true}}
+          render={({field})=>(
+            <TextField 
+            type={"password"}
+            fullWidth
+            name='password'
+            {...field}
+            error={!!errors['password']}
+            helperText={errors['password'] ? errors['password'].message : ''}
+            // helperText={errors?.password ? errors?.password?.message : ""}
+            // onChange={(e) => setPassword(e.target.value)} 
+            id="outlined-basic" 
+            label="Password" 
+            variant="outlined"
+            />
+          )}
+          />
+
+      
 
       <Button 
       variant='contained'
@@ -122,6 +157,7 @@ const Login = () => {
       Let's Go
       </Button>
       
+      
 
       <Typography>
         <Link to="forgotpassword">Forgot Password?</Link>
@@ -129,7 +165,7 @@ const Login = () => {
       <Link style={{textDecoration:"none"}} to="register"><Button>Registration</Button></Link>
       {loader ? <LinearProgress sx={{ width: '100%' }}/> : <></>}
       </Box>
-      </form>
+      
     </div>
   )
 }
